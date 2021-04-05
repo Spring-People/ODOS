62b85d25479095a8010c0793134f5d466463aa68
Yanjoo <hanjub3@gmail.com>
2021-04-06 00:53:53 +0900

    refactor: move getLoginInfo method to login package

--- src/main/java/com/my/odos/login/controller/LoginController.java
+++ src/main/java/com/my/odos/login/controller/LoginController.java
@@ -1,16 +1,17 @@
 package com.my.odos.login.controller;
 
 import com.my.odos.exception.WrongIdPasswordException;
+import com.my.odos.login.vo.LoginMemberInfo;
 import com.my.odos.login.service.AuthService;
 import com.my.odos.login.vo.AuthInfo;
 import com.my.odos.login.vo.LoginCommand;
-import com.my.odos.register.vo.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -38,7 +39,6 @@ public class LoginController {
             AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 
             session.setAttribute("authInfo", authInfo);
-
              /*
                 1. 이메일로 DB에서 객체 조회
                 2. 객체에서 connected 변수 상태 확인
@@ -56,6 +56,22 @@ public class LoginController {
         } catch (WrongIdPasswordException e){
             return "login/loginForm";
         }
+    }
+
+    @GetMapping("/api/receiveLoginInfo")
+    @ResponseBody
+    public LoginMemberInfo getLoginInfo(HttpSession session) {
+        /*
+         * session에서 현재 로그인한 member의 id를 리턴
+         * */
+        AuthInfo currentUser = (AuthInfo) session.getAttribute("authInfo");
+        String id = String.valueOf(currentUser.getId());
+        System.out.println(id);
+
+        LoginMemberInfo info = new LoginMemberInfo();
+        info.setId(id);
+
+        return info;
     }
 
 }
