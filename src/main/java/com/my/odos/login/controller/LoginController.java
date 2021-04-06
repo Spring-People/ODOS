9691f295fc7eff2be0f7071b5cec5be975aab56d
genius-jo <joks7285@daum.net>
2021-04-07 04:00:25 +0900

    style change autowired to requiredargsconstructor

--- src/main/java/com/my/odos/login/controller/LoginController.java
+++ src/main/java/com/my/odos/login/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.my.odos.login.vo.LoginMemberInfo;
 import com.my.odos.login.service.AuthService;
 import com.my.odos.login.vo.AuthInfo;
 import com.my.odos.login.vo.LoginCommand;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,15 +17,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
 
-    private AuthService authService;
-
-    @Autowired
-    public void setAuthService(AuthService authService) {
-        this.authService = authService;
-    }
+    private final AuthService authService;
 
     @GetMapping
     public String form(Model model){
@@ -39,20 +36,14 @@ public class LoginController {
             AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
 
             session.setAttribute("authInfo", authInfo);
-             /*
-                1. 이메일로 DB에서 객체 조회
-                2. 객체에서 connected 변수 상태 확인
-                3. connected = false 일 경우 초대장 페이지로 이동
-            */
             String email = loginCommand.getEmail();
             boolean isConnectedMember = authService.isConnetcedMember(email);
+
             if (!isConnectedMember) {
-                System.out.println("초대장 페이지 이");
-                return "redirect:invitation/Invitation.html";
-//                return "invitation/Invitation";
+                return "redirect:invitation/Invitation.html"; //초대장 페이지 이동
             }
 
-            return "login/loginSuccess";
+            return "main/main";
         } catch (WrongIdPasswordException e){
             return "login/loginForm";
         }
@@ -61,12 +52,10 @@ public class LoginController {
     @GetMapping("/api/receiveLoginInfo")
     @ResponseBody
     public LoginMemberInfo getLoginInfo(HttpSession session) {
-        /*
-         * session에서 현재 로그인한 member의 id를 리턴
-         * */
+
+        //session에서 현재 로그인한 member의 id를 리턴
         AuthInfo currentUser = (AuthInfo) session.getAttribute("authInfo");
         String id = String.valueOf(currentUser.getId());
-        System.out.println(id);
 
         LoginMemberInfo info = new LoginMemberInfo();
         info.setId(id);
