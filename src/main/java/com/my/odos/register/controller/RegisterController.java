980d1b4513594185b92a0b06cdd49099123be5bf
genius-jo <joks7285@daum.net>
2021-04-07 03:51:01 +0900

    style : change autowired to requireargsconstructor

--- src/main/java/com/my/odos/register/controller/RegisterController.java
+++ src/main/java/com/my/odos/register/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.my.odos.register.controller;
 import com.my.odos.register.service.RegisterService;
 import com.my.odos.register.vo.RegisterRequest;
 import javassist.bytecode.DuplicateMemberException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class RegisterController {
 
-    private RegisterService registerService;
+    private final RegisterService registerService;
 
-    @Autowired
-    public void setRegisterService(RegisterService registerService) {
-        this.registerService = registerService;
-    }
-
+    //약관 동의
     @RequestMapping("/register/step1")
     public String handleStep1(){
         return "register/step1";
@@ -28,25 +26,25 @@ public class RegisterController {
     @PostMapping("/register/step2")
     public String handleStep2(@RequestParam(value="agree", defaultValue = "false") Boolean agreeVal, Model model){
 
-        if(!agreeVal){
-            return "register/step1";
-        }
+        //동의하지 않았을때
+        if(!agreeVal) { return "register/step1"; }
+
         model.addAttribute("registerRequest",new RegisterRequest());
+
         return "register/step2";
     }
 
     @PostMapping("/register/step3")
     public String handleStep3(RegisterRequest regReq){
 
-        if(regReq.isPasswordEqualToConfirmPassword() == false) {
-            return "register/step2";
-        }
+        //비밀번호 확인한게 다를때
+        if(!regReq.isPasswordEqualToConfirmPassword()) { return "register/step2"; }
 
         try{
             registerService.regist(regReq);
-            return "register/step3";
+            return "register/step3"; //가입 완료 창
         } catch (DuplicateMemberException ex) {
-            return "register/step2";
+            return "register/step2"; //이미 있는 아이디일때
         }
     }
 }
