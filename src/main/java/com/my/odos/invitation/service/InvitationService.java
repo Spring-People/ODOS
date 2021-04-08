40fc150a84cfc941644a955ab9c7750d3ac809f8
Yanjoo <hanjub3@gmail.com>
2021-04-08 13:33:03 +0900

    refactor: move makeTeam feature from InvitationService to TeamService

--- src/main/java/com/my/odos/invitation/service/InvitationService.java
+++ src/main/java/com/my/odos/invitation/service/InvitationService.java
@@ -7,6 +7,7 @@ import com.my.odos.exception.InvitationException;
 import com.my.odos.invitation.repository.InvitationRepository;
 import com.my.odos.invitation.repository.MemberRepository;
 import com.my.odos.invitation.repository.TeamRepository;
+import com.my.odos.team.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ public class InvitationService {
     private final InvitationRepository invitationRepository;
     private final MemberRepository memberRepository;
     private final TeamRepository teamRepository;
+    private final TeamService teamService;
 
     public Boolean createInvitation(int fromId, int toId) {
         Invitation check = invitationRepository.findByToIdAndFromId(toId, fromId);
@@ -61,15 +63,8 @@ public class InvitationService {
         );
         receiver.setConnected(true);
 
-        //팀을 만들고 sender와 receiver를 지금 만들어지는 팀에 연결
-        Team team = new Team();
-        team.setUploadTime(LocalTime.of(1,0,0));
-        teamRepository.save(team);
-
-        sender.setGroupId(team.getId());
-        receiver.setGroupId(team.getId());
-
-
+        teamService.makeTeam(sender, receiver);
+        
         return true;
     }
 }
