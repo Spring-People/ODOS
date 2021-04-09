daf40e13956d19da1b8a2f0726b37d606a4145aa
Yanjoo <hanjub3@gmail.com>
2021-04-09 17:38:54 +0900

    add style to receive invitation page

--- src/main/resources/static/invitation/receive.js
+++ src/main/resources/static/invitation/receive.js
@@ -19,33 +19,29 @@ $(document).ready(function () {
         success: function (response) {
             console.log(response);
             let itemDto = response;
-            list.append(addHTML(itemDto));
+            // list.append(addHTML(itemDto));
+            $('#invitation-exist').removeClass('hide');
+            $('#invitation-none').addClass('hide');
+            $('#sender-name').text(itemDto.name);
+            $('#receive-button').on('click', function() {
+                let from_id = itemDto.id;
+                let data = {'toId': currentId, 'fromId': from_id}
+                $.ajax({
+                    type: 'POST',
+                    url: '/api/receiveInvitation',
+                    contentType: 'application/json',
+                    data: JSON.stringify(data),
+                    success: function (response) {
+                        console.log("메인으로 이동");
+                        window.location.href = "/main";
+                    }
+                });
+            });
+
         }
     });
 });
 
-function addHTML(itemDto) {
-    return `
-        <li>${itemDto.name}</li>
-        <button onclick="makeConnection(${itemDto.id})">초대장 수락</button>
-    `;
-}
 
-function makeConnection(from_id) {
-    console.log(currentId, from_id);
-    let data = {'toId': currentId, 'fromId': from_id}
-    $.ajax({
-        type: 'POST',
-        url: '/api/receiveInvitation',
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        success: function (response) {
-            console.log("메인으로 이동");
-            window.location.href = "/main";
-        }
-    });
-
-
-}
 
 
